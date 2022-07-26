81c3f3d8de742dfbfa23e89d2817611ce50ac0c6
ShalviMarudkar <shalvimarudkar27@gmail.com>
2022-07-26 13:11:29 +0530

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
 import Home from "./components/Home";
 import NavBar from './components/NavBar/NavBar';
+import Login from "./components/LogIn/login";
 
 import './App.css'  
 
@@ -13,7 +15,7 @@ const App = () => {
                 <NavBar />
                 <Routes>
                     <Route path='/' exact element={<Home />}/>
-                    <Route path='/login' exact />
+                    <Route path='/login' exact element= {<Login />} />
                 </Routes>
             </BrowserRouter>
         </>
