e7adb341fa4cbfdd2ceb105e377610a46b50adea
fleaBag13 <dyutika.khatate@gmail.com>
2022-07-26 17:49:23 +0530

    Fixed login form

--- frontend/src/components/LogIn/login.js
+++ frontend/src/components/LogIn/login.js
@@ -1,23 +1,39 @@
 import React, {useState} from 'react'
 import "./login.css";
 import { signin } from '../../api';
+import { Navigate } from 'react-router-dom';
 
 const initialState = {username: '', password: ''};
 
 const Login = () => {
+
+    const [form, setForm] = useState(initialState);
+    const [showPassword, setShowPassword] = useState(false);
+    const handleShowPassword = () => setShowPassword(!showPassword);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        signin(form);
+
+        Navigate('/principal');
+    }
+
+    const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
     return(
+       <form onSubmit={handleSubmit}> 
       <div id="loginform">
         <h2 id="headerTitle">Welcome to Events-N-You</h2>
         <div>
 
           <div class="row">
             <label>Username</label>
-            <input type="text" placeholder="Enter your Username"/>
+            <input type="text" placeholder="Enter your Username" handleChange={handleChange}/>
           </div>
 
           <div class="row">
             <label>Password</label>
-            <input type="password" placeholder="Enter your Password"/>
+            <input placeholder="Enter your Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword}/>
           </div>
 
           <div id="button" class="row">
@@ -26,6 +42,7 @@ const Login = () => {
 
         </div>
       </div>
+      </form>
     );
 }
 
