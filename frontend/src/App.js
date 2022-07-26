f5a0071831dac931be03aaf180622cbc227194f3
fleaBag13 <dyutika.khatate@gmail.com>
2022-07-26 21:21:19 +0530

    abc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Redirect, useLocation } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 
 import Home from "./components/Home";
 import NavBar from './components/NavBar/NavBar';
@@ -9,14 +10,28 @@ import Login from "./components/LogIn/login";
 import './App.css'  
 
 const App = () => {
+    const user = JSON.parse(localStorage.getItem('profile'));
+    // const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    // const location = useLocation();
+
+    // useEffect (() => {
+    //     // const token = user?.token;
+    //     // if(token){
+    //     //     const decodedToken = decode(token);
+    //     //     if(decodedToken.exp*1000 < new Date().getTime()) logout();
+    //     // }
+    //     setUser(JSON.parse(localStorage.getItem('profile')));
+    // }, [location]);
     return(
         <>            
             <BrowserRouter>
                 <NavBar />
                 <Routes>
                     <Route path='/' exact element={<Home />}/>
-                    <Route path='/login' exact element= {<Login />} />
+                    <Route path='/login' exact element= {(!user) ? <Login />: <Navigate to="/principal"/>} />
                     
+                    <Route path='/principal' exact element= {(!user) ? <Login />: <Home />}/> 
+                    <Route path='/committee' exact element= {(!user) ? <Login />: <Home />}/>
                 </Routes>
             </BrowserRouter>
         </>
--- frontend/src/components/LogIn/login.js
+++ frontend/src/components/LogIn/login.js
@@ -14,7 +14,7 @@ const Login = () => {
         e.preventDefault();
 
         signin(form);
-
+        
         Navigate('/principal');
     }
 
