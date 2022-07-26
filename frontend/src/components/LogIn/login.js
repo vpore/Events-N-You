83b366f1498a3deb7324bda6873683877a118a36
ShalviMarudkar <shalvimarudkar27@gmail.com>
2022-07-26 15:20:38 +0530

    added login page

--- frontend/src/components/LogIn/login.js
+++ frontend/src/components/LogIn/login.js
@@ -1,28 +1,43 @@
 import React from 'react'
 import { useState } from "react";
-const Login= () =>{
-    const [ email, setEmail] = useState("");
-    const [ password, setPassword] = useState("");
-    return(
-        <form action= "">
-            
-                <h2>Welcome to Events-N-You </h2>
-                <div className= "form-group">
-                    <label htmlFor= "email">Email </label>
-                    <input type="email" name="email" id="email"/>
-                       
-                </div>
-                <div className= "form-group">
-                    <label htmlFor="password">Password </label>
-                    <input type="password" name="password" id="password"/>
-                   </div>
-                <button type ="submit">Login</button>
+import "./login.css";
 
-            
-        </form>
+class Login extends React.Component{
+  render(){
+    return(
+      <div id="loginform">
+        <FormHeader title="Welcome to Event-N-You" />
+        <Form />
+      </div>
     )
-       
-    
+  }
 }
+
+const FormHeader = props => (
+    <h2 id="headerTitle">{props.title}</h2>
+);
+
+
+const Form = props => (
+   <div>
+     <FormInput description="Username" placeholder="Enter your username" type="text" />
+     <FormInput description="Password" placeholder="Enter your password" type="password"/>
+     <FormButton title="Log in"/>
+   </div>
+);
+
+const FormButton = props => (
+  <div id="button" class="row">
+    <button>{props.title}</button>
+  </div>
+);
+
+const FormInput = props => (
+  <div class="row">
+    <label>{props.description}</label>
+    <input type={props.type} placeholder={props.placeholder}/>
+  </div>  
+);
+
 
 export default Login
